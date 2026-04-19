8f22a16e008d03b6eaef1c0f6e80de860144869a
Raafatalhalaki-glitch <raafat2015alhalaki@gmail.com>
2026-04-19 23:31:05 +0300

    Update server.js

--- server.js
+++ server.js
@@ -3,6 +3,12 @@ const path = require('path');
 const app = express();
 
 app.use(express.json({ limit: '50mb' }));
+app.use((req, res, next) => {
+  if (req.path.endsWith('.json')) {
+    res.setHeader('Content-Type', 'application/json');
+  }
+  next();
+});
 app.use(express.static(path.join(__dirname, 'public')));
 
 // Rate limiting بسيط
